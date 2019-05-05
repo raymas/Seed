1200d983c862ec8c6ebd667b017f4e80ac03c1f9
raymas <cSYvL6@yandex.com>
2019-05-05 22:42:47 +0200

    Emmiter and receiver scripts

---
+++ LoRaReceiverESP32.ino
@@ -0,0 +1,74 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include "SSD1306.h"
+
+
+// GPIO5  -- SX1278's SCK
+// GPIO19 -- SX1278's MISO
+// GPIO27 -- SX1278's MOSI
+// GPIO18 -- SX1278's CS
+// GPIO14 -- SX1278's RESET
+// GPIO26 -- SX1278's IRQ(Interrupt Request)
+
+// LORA paramters must be the same as the sender
+#define SS      18
+#define RST     14
+#define DI0     26
+#define BAND    433E6
+
+SSD1306  display(0x3c, 4, 15);
+
+String v = "";
+
+void setup() {
+  Serial.begin(115200);
+  // while (!Serial); //if just the the basic function, must connect to a computer
+  delay(1000);
+
+  
+  Serial.println("LoRa Receiver"); 
+  
+  SPI.begin(5,19,27,18);
+  LoRa.setPins(SS,RST,DI0);
+  
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa init completed!")
+  
+  pinMode(16,OUTPUT);
+  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  delay(50); 
+  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
+
+  display.init();
+
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    v = "";
+    // read packet
+    while (LoRa.available()) {
+      v += (char)LoRa.read();
+      display.clear();
+      display.drawString(0, 1, "LoRa Receiver");
+      display.drawString(0, 10, v);
+      display.display();
+    }
+
+    Serial.print(v);
+    
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }  
+}
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Seed
+
+ESO's Seed project. Point to point telemetry is here!
+Mostly in C language.
+
+## Getting started
+
+The [LoRaReceiverESP32](LoRaReceiverESP32.ino) is to flash on the receiver unit.
+The [LoRaSender](LoRaSender.ino) is to flash on the emmiter unit.
+
+You have to respect the wiring diagram, because you know there are some sensors!
+
+To implement that, we need a library for that kind of ESP32 Lora TTL which is available at [drive.google.com](https://drive.google.com/file/d/1L8ll-DeAC2SATBZn0-KbSaZsrinbnXkF/view).
+You to download that big *.rar file and open it with a unrar program like 7zip.
+Unzip the TTGO WIFI-OLED.zip and you get the TTGO WIFI-OLED folder.
+
+Move the TTGO WIFI-OLED to yours Arduino's hardware folder in your Documents.
+
+It looks something like this :
+
+![arduino_path](ressources/arduino_path.jpg)
+
+Restart Arduino and check in Tools -> Board that you have now some TTGO's board
+
+## Installing and flashing
+
+Clone this git or download it as a zip
+
+```bash
+git clone https://github.com/raymas/Seed.git
+```
+
+Open the [LoRaSender](LoRaSender.ino) script with arduino, upload it with the board : EPS8266 with defaults parameters. Don't forget to select the correct port.
+And here we go again with the other unit, flash it with the [LoRaReceiverESP32](LoRaReceiverESP32.ino) script.
+
+Power up the two modules and observe some messages displayed on the screen.
